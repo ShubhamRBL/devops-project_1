f889c2cc9225a273eaeb184381e715dadc54bad4
shubham <sapatil1302@gmail.com>
2025-07-20 17:37:04 +0530

    first

---
+++ devops.py
@@ -0,0 +1,15 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/info')
+def home():
+    return "Hello, My name is shubham. This is my 1st project."
+
+@app.route('/address')
+def address():
+    return "I am from Mumbai, Maharashtra, India"
+
+
+app.run(host='0.0.0.0', port=5000, debug=True)
+
